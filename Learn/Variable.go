d75ed3a3d5eeeffcb0bc613718306b7b0a05f1b8
JJLee-20194099 <JJLee-20194099>
2021-10-13 14:31:31 +0700

    add strconv package to convert number to string

--- Learn/Variable.go
+++ Learn/Variable.go
@@ -1,8 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+import "strconv"
 
 var (
 	n int = 19
@@ -14,14 +13,23 @@ var (
 var N int = 10
 
 func main() {
-	var i int = 10;
-	var s string;
-	s = "JJLee";
-	fmt.Println(i);
-	fmt.Println(s);
-	k := true
-	fmt.Printf("%v, %T\n", k, k);
-	var n int = 1000
-	fmt.Println(n);
+	// var i int = 10;
+	// var s string;
+	// s = "JJLee";
+	// fmt.Println(i);
+	// fmt.Println(s);
+	// k := true
+	// fmt.Printf("%v, %T\n", k, k);
+	// var n int = 1000
+	// fmt.Println(n);
 	// shadow variable n
+
+	var i int = 1000;
+	// var j float32 = float32(i);
+	var str = strconv.Itoa(i);
+	// var t = string(i);
+	fmt.Printf("%v, %T\n", str, str);
+	// fmt.Printf("%v, %T\n", t, t);
+
+
 }
