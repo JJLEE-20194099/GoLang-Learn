b388868f28e96f025e80b13a16735114332b67ec
JJLee-20194099 <JJLee-20194099>
2021-10-13 14:54:18 +0700

    string byte rune in golang

--- Learn/primitive-datatypes.go
+++ Learn/primitive-datatypes.go
@@ -8,6 +8,18 @@ func main() {
 	fmt.Printf("%v, %T\n", real(a), imag(a));
 	fmt.Printf("%v, %T\n", real(b), imag(b));
 
+	var c byte = 'H';
+	fmt.Printf("%v, %T\n", c, c);
+	var str string = "Hello world";
+	var newStr []byte = []byte(str);
+	fmt.Printf("%v, %T\n", newStr, newStr);
+
+	var runeVar rune = 'e';
+	fmt.Printf("%v, %T", runeVar, runeVar);
+
+	
+
+
 }
 
 
